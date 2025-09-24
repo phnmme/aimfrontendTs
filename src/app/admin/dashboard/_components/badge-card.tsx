f2ee7f5fe13f13378533e887fa62e899daf848c8
Chitchai-Jantanarak <s6604062663051@email.kmutnb.ac.th>
2025-09-24 21:11:26 +0700

    add: badging as type

---
+++ src/app/admin/dashboard/_components/badge-card.tsx
@@ -0,0 +1,18 @@
+import { Badge } from "@/components/ui/badge";
+
+export type BadgeType = "someone" | "something"; 
+// TODO: specify based on that system design
+export type ExpireDayRange = {
+    5: "bg-red",
+    30: "bg-yellow",
+    _: ""
+};
+
+// TODO: Injectable
+export default function BadgeCard() {
+    return (
+        <Badge variant="outline">
+            ทดสอบ
+        </Badge>
+    )
+}
