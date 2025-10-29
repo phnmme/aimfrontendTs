5bdaba454d2321928f42ea84e34634ff4f063a0a
6604062620204 <s6604062620204@email.kmutnb.ac.th>
2025-10-29 22:48:15 +0700

    refactor: improve readability of dashboardGetSummaryAction by restructuring fetch request

--- src/actions/dashborad-action.ts
+++ src/actions/dashborad-action.ts
@@ -1,16 +1,14 @@
 export async function dashboardGetSummaryAction() {
   const token = localStorage.getItem("token");
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_HOST_URL}
-api/v1/analysis/authorized/summary`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    }
-  );
+  const host = process.env.NEXT_PUBLIC_HOST_URL;
+  console.log("Host URL:", host);
+  const res = await fetch(host + "api/v1/analysis/authorized/summary", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
   const data = await res.json();
   if (!res.ok) {
     return { success: false, message: data.message };
